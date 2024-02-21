b932e2828f34d275f3c6e96373e8f947fcc375b5
Zenlac <caleleizengrillera@gmail.com>
2024-02-21 10:06:13 +0800

    Second Commit

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Routing\Controller as BaseController;
+
+class ProductController extends BaseController
+{
+    public function index1()
+    {
+        $data = [
+            'Gadget' => name ' 
+            
+        ]
+    }
+}
+{
+    use AuthorizesRequests, ValidatesRequests;
+}
